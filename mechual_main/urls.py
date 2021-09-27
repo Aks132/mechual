1d891e66aef43784a4dedb616337ce98c0e7ccf1
Aks132 <achyutsapariya@gmail.com>
2021-09-27 22:56:19 +0530

    path added

--- mechual_main/urls.py
+++ mechual_main/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('index.html', views.index, name='index'),
     path('about.html', views.about, name='about'),
     path('contact.html', views.contact, name='contact'),
