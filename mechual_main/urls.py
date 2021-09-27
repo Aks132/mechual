c48971e9a587a34400c366c01aa3c6336eb84854
Aks132 <achyutsapariya@gmail.com>
2021-09-27 22:48:00 +0530

    path added

--- mechual_main/urls.py
+++ mechual_main/urls.py
@@ -5,5 +5,10 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('index.html', views.index, name='index'),
+    path('about.html', views.about, name='about'),
+    path('contact.html', views.contact, name='contact'),
+    path('events.html', views.events, name='events'),
+    path('single.html', views.events, name='page'),
+
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- mechual_main/views.py
+++ mechual_main/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'mechual_main/index.html')
+
+def about(request):
+    return render(request, 'mechual_main/about.html')
+
+def contact(request):
+    return render(request, 'mechual_main/contact.html')
+
+def events(request):
+    return render(request, 'mechual_main/events.html')
+
+def single(request):
+    return render(request, 'mechual_main/single.html')
